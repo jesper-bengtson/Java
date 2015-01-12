6e4a0ed7324c40ff3f91300626d541299e8b2ed3
jesper-bengtson <jebe@itu.dk>
2015-01-12 01:45:16 +0100

    Added files

---
+++ Java/src/Java/Logic/ProtocolLogic.v
@@ -0,0 +1,164 @@
+Require Import Rel Open Later ILogic ILInsts BILogic BILInsts IBILogic SepAlg SepAlgMap
+               UUSepAlg SepAlgInsts OpenILogic Pure ILEmbed PureInsts Subst Stack.
+Require Import Maps MapInterface MapFacts.
+Require Import Lang SessionType Util Program.
+
+  Local Existing Instance ILPre_Ops.
+  Local Existing Instance ILPre_ILogic.
+  Local Existing Instance EmbedOpId.
+  Local Existing Instance EmbedId.
+  Local Existing Instance ILFun_Ops.
+  Local Existing Instance ILFun_ILogic.
+  
+  Local Existing Instance ILLaterNat.
+  Local Existing Instance ILLaterNatOps.
+  Local Existing Instance ILLaterPre.
+  Local Existing Instance ILLaterPreOps.
+  Local Existing Instance ILLaterFun.
+  Local Existing Instance ILLaterFunOps.
+  
+  Local Existing Instance SAIBIOps.
+  Local Existing Instance SAIBILogic.
+  Local Existing Instance BILFun_Ops.
+  Local Existing Instance BILPre_Ops.
+  Local Existing Instance IBILPreLogic.
+  Local Existing Instance BILFun_Ops.
+  Local Existing Instance BILFunLogic.
+  Local Existing Instance IBILFunLogic.
+
+  Local Existing Instance MapSepAlgOps.
+  Local Existing Instance MapSepAlg.
+  Local Existing Instance MapEquiv.
+  Local Existing Instance EquivPreorder.
+  Local Existing Instance UUMapSepAlg.
+  Local Existing Instance SepAlgOps_prod.
+  Local Existing Instance SepAlg_prod.
+  Local Existing Instance UUSepAlg_prod.
+  
+  Local Existing Instance EmbedILFunDropOp.
+  Local Existing Instance EmbedILFunDrop.
+  Local Existing Instance EmbedILFunOp.
+  Local Existing Instance EmbedILFun.
+  Local Existing Instance EmbedILPreOp.
+  Local Existing Instance EmbedILPre.
+
+  Local Existing Instance SABIOps.
+  Local Existing Instance SABILogic.
+  Local Existing Instance pureop_bi_sepalg.
+
+  Local Transparent ILPre_Ops.
+  Local Transparent ILFun_Ops.
+  Local Transparent BILPre_Ops.
+  Local Transparent BILFun_Ops.
+
+Require Import AssertionLogic.
+
+Check (@Equal protocol _ _ ST).
+Print protocol.
+Locate ST.
+  Definition pasn := ILPreFrm (@rel protocol (@Equal channel _ _ ST)) asn.
+  
+  Instance ILogicOpsPAsn    : ILogicOps pasn    := _.
+  Instance ILogicPAsn       : ILogic pasn       := _.  
+  Instance BILOperatorsPAsn : BILOperators pasn := @SABIOps2 protocol Equal _ _ asn _ _ _.
+  
+  Instance BILogicPAsn      : BILogic pasn := @SABILogic2 protocol Equal _ _ asn _ _ _ _ _.
+
+  Instance EmbedPAsnPropOp  : EmbedOp Prop pasn := @EmbedILPreDropOp protocol Equal Prop asn _ _ _.
+  Instance EmbedPAsnProp    : Embed Prop pasn   := @EmbedILPreDrop protocol Equal _ Prop _ asn _ _ _ _.
+    
+  Instance EmbedPAsnAsnOp   : EmbedOp asn pasn  := @EmbedILPreDropOp protocol Equal asn asn _ _ _.
+  Instance EmbedPAsnAsn     : Embed asn pasn    := @EmbedILPreDrop protocol Equal _ asn _ asn _ _ _ _.
+  
+  Definition psasn := @open var val pasn.
+
+  Instance ILogicOpsPSasn    : ILogicOps psasn    := _.
+  Instance ILogicPSasn       : ILogic psasn       := _.
+  Instance BILOperatorsPSasn : BILOperators psasn := _.
+  Instance BILogicPSasn      : BILogic psasn      := _.
+  Instance IBILogicPSasn     : BILogic psasn     := _.
+
+  Instance EmbedPSasnPropOp : EmbedOp Prop psasn := _.
+  Instance EmbedPSasnProp   : Embed Prop psasn   := _.
+
+  Instance EmbedPSasnPureOp : EmbedOp vlogic psasn := _.
+  Instance EmbedPSasnPure   : Embed vlogic psasn   := _.
+
+  Instance EmbedPSasnAsnOp : EmbedOp asn psasn := _. 
+  Instance EmbedPSasnAsn   : Embed asn psasn := _.
+
+  Instance EmbedPSasnSasnOp : EmbedOp sasn psasn := _. 
+  Instance EmbedPSasnSasn   : Embed sasn psasn := _.
+  
+Require Import Heap.  
+
+Definition mk_pasn (f: protocol -> heap -> Prop)
+  (HProt: forall Pr Pr' h, Pr === Pr' -> f Pr h -> f Pr' h)
+  (Hheap: forall Pr h h', subheap h h' -> f Pr h -> f Pr h') : pasn.
+  refine (mkILPreFrm (fun Pr => mk_asn (f Pr) (Hheap Pr)) _).
+Proof.
+  intros Pr Pr' HPr.
+  simpl; intros; eapply HProt; eassumption.
+Defined.
+
+  Program Definition has_ST (v : var) (T : ST) : psasn :=
+    fun s => mk_pasn (fun p h =>
+      MapsTo (val_to_channel (s v)) T p
+    ) _ _.
+  Next Obligation.
+	intros.
+	rewrite <- H.
+	assumption.
+  Defined.
+
+  Program Definition has_subst_ST (x z y : var) (T : ST) : psasn :=
+    fun s => mk_pasn (fun p h =>
+      MapsTo (val_to_channel (s x)) (subst_ST z (s y) T) p
+    ) _ _.
+  Next Obligation.
+    rewrite <- H; assumption.
+  Defined.
+
+  Program Definition all_STs (T : ST) : psasn :=
+    fun s => mk_pasn (fun p h =>
+      forall c, In c p -> MapsTo c T p
+    ) _ _.
+  Next Obligation.
+	rewrite <- H; apply H0; rewrite H. assumption.
+  Qed.
+  
+  (*
+  Import Marshall.
+  Program Definition isMarshallable (v : var) (P : sasn) : psasn :=
+  	fun s PM STs => mk_asn (fun Pr k h =>
+    	forall m h' Pr' k' (Hsh: subheap h h') (Hsp: Prog_wf_sub Pr Pr'), 
+    	marshall Pr' (s v) h' m -> P s Pr' k' m
+  	) _ _ _.
+  Next Obligation.
+    eapply H0; try eassumption.
+    transitivity P'; assumption.
+  Defined.
+  Next Obligation.
+    eapply H0; [ | assumption | eassumption].
+    transitivity h'; eassumption.
+  Defined.
+  *)
+  
+  (*
+  Program Definition DecidableAsn (P : sasn) : sasn := 
+    fun s => mk_asn (fun Pr n h =>
+     P s Pr n h \/ ~(P s Pr n h)
+   ) _ _ _.
+  Next Obligation.
+    destruct H. left.
+    solve_model H.
+    right; intro Hfail; apply H. solve_model Hfail.
+  Program Definition DecidableAsn (P : sasn) : sasn := 
+    fun s' => mk_asn (fun Pr' n' h' =>
+      forall s Pr n h, P s Pr n h \/ ~(P s Pr n h)
+   ) _ _ _.
+  *)
+  (*
+  Definition DecidableAsn (P : asn) := forall Pr n h, P Pr n h \/ ~(P Pr n h).
+  Definition DecidableSasn (P : sasn) := forall s, DecidableAsn (P s).
+*)
---
+++ Java/src/Java/Logic/SessionType.v
@@ -0,0 +1,70 @@
+Require Import ILogic ILInsts SepAlg BILogic BILInsts IBILogic SepAlgMap Maps String Rel ZArith.
+Require Import RelationClasses Setoid Morphisms. 
+Require Import MapInterface MapFacts.
+Require Import Open Stack Subst Lang OpenILogic Pure ILEmbed PureInsts.
+Require Import UUSepAlg SepAlgInsts.
+Require Import Heap AssertionLogic Util Program.
+
+Inductive ST : Type :=
+  | st_end : ST
+  | st_send : var -> sasn -> ST -> ST
+  | st_recv : var -> sasn -> ST -> ST.
+  
+Definition protocol := Map [channel, ST].
+
+Local Existing Instance MapSepAlgOps.
+Local Existing Instance MapSepAlg.
+Local Existing Instance MapEquiv.
+Local Existing Instance EquivPreorder.
+Local Existing Instance UUMapSepAlg.
+
+Instance RelSTs : Rel protocol := _.
+Instance PreorderSTs : PreOrder (@rel protocol RelSTs) := _.
+Instance STsSepAlgOps : SepAlgOps protocol := _.
+Instance SepAlgSTs : SepAlg protocol := _.
+Instance UUSepAlgSTs : UUSepAlg protocol := _.
+
+Fixpoint dual_ST (st : ST) : ST :=
+  match st with
+  | st_end => st_end
+  | st_send v P st' => st_recv v P (dual_ST st')
+  | st_recv v P st' => st_send v P (dual_ST st')
+  end.
+(*
+Program Instance ST_Dual : Dual ST dual_ST := {
+  dual_involutive := _
+}.
+Next Obligation.
+  induction a; [ reflexivity | |];
+  simpl; rewrite IHa; reflexivity.
+Defined.
+*)
+Fixpoint subst_ST (x : var) (v : val) (t : ST) : ST :=
+  match t with
+  | st_end => st_end
+  | st_send x' A t' => st_send x' (apply_subst A (subst1 `v x)) (subst_ST x v t')
+  | st_recv x' A t' => st_recv x' (apply_subst A (subst1 `v x)) (subst_ST x v t')
+  end.
+
+Fixpoint ST_append (l r : ST) : ST :=
+  match l with
+    | st_end => r
+    | st_send v P l' => st_send v P (ST_append l' r)
+    | st_recv v P l' => st_recv v P (ST_append l' r)
+  end.
+
+Definition protocol_append (l r : protocol) : protocol :=
+  mapi (fun k r' => match (find k l) with
+    | Some l' => ST_append l' r'
+    | None => r'
+    end) r. 
+
+Module STNotations.
+  	(*Notation " x ':' t " := (has_ST x t) (at level 88, left associativity) : st_scope.*)
+  	
+	Notation " '!' x ',{' P '}.' t " := (st_send x P t) (at level 88, left associativity) : st_scope.
+	Notation " '&' x ',{' P '}.' t " := (st_recv x P t) (at level 88, left associativity) : st_scope.
+
+	Open Scope st_scope.
+
+End STNotations.
